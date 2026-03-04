3a2a3f80f7bb6c9f814a6fa136a366dbebd564fc
Rafael Martins Nogueira <rafamartnogueira@gmail.com>
2026-03-03 22:30:52 -0300

    pagina de login

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+
+  return(
+  <div className="w-screen h-screen bg-[#4D0012] flex items-center justify-center" >
+    <div className="bg-white p-8 rounded shadow w-96 h-96 ">
+      <h1>Hello world</h1>
+    <button className="bg-red-500 font-bold text-xl hover:animate-bounce px-2 py-1 ">
+      Me aperte!! 
+    </button>
+    </div>
+  </div>
+  )
+}
+
+export default App
